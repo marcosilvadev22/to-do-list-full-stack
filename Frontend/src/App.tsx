97a3733952e4d98c65431b587f8b760faa3c00bb
rielisson <118862671+rielisson@users.noreply.github.com>
2026-08-13 19:36:47 -0300

    implemetei o protectedRoute, rota que protege de acesar certos componentes sem autenticação

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,17 +2,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/initialPage/Home";
 import Login from "./components/initialPage/Login";
 import Dashboard from "../src/components/dashbord/Dashbord"; // ajuste o caminho conforme seu projeto // ajuste o caminho conforme seu projeto
-// import ProtectedRoute from "./routes/ProtectedRoute";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login onLogin={() => {}} />} />
+      <Route path="/login" element={<Login onLogin={() => { }} />} />
       <Route
         path="/dashboard"
         element={
+          <ProtectedRoute>
             <Dashboard />
+          </ProtectedRoute>
+
         }
       />
       <Route path="*" element={<Navigate to="/" replace />} />
